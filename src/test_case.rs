6f56cc5e6bc99792b891c00eb60abf733cf1901c
ravindranath.m <ravindranath.m@comviva.com>
2024-04-03 16:55:42 +0530

    Handles bearer token authorization for API.

--- src/test_case.rs
+++ src/test_case.rs
@@ -1,4 +1,5 @@
 use colored::{self, Colorize};
+use serde::Deserialize;
 use std::error::Error;
 
 use reqwest::{
@@ -19,6 +20,8 @@ pub struct TestCase {
     pub headers: Vec<(String, String)>,
     pub payload: Value,
     pub expected_status: StatusCode,
+    is_authorizer: bool,
+    is_authorized: bool,
 
     // fields that will be filled after test case is executed..
     pub actual_status: Option<reqwest::StatusCode>,
@@ -38,6 +41,8 @@ impl TestCase {
             headers: Vec::new(),
             payload: Value::Null,
             expected_status: StatusCode::OK,
+            is_authorized: false,
+            is_authorizer: false,
 
             actual_status: Option::<reqwest::StatusCode>::None,
             response: serde_json::Value::Null,
@@ -81,18 +86,58 @@ impl TestCase {
             .map(|s| StatusCode::from_u16(s).unwrap())
             .unwrap_or(StatusCode::OK);
 
+        let is_authorizer_str = row[10].get_string().unwrap_or("").to_lowercase();
+        test_case.is_authorizer = match is_authorizer_str.as_str() {
+            "authorizer" => true,
+            _ => false, // default to false if the value is not "authorizer"
+        };
+        test_case.is_authorized = match is_authorizer_str.as_str() {
+            "authorized" => true,
+            _ => false, // default to false if the value is not "authorized"
+        };
+
         Ok(test_case) // return the populated test case
     }
 
-    pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
+    /* Run the test case by invoking the appopriate http method */
+    pub fn run(&mut self, jwt_token: Option<String>) -> Result<Option<String>, Box<dyn Error>> {
         println!("Running the test case: {}", self.name);
         let client = reqwest::blocking::Client::new();
-        let mut headers = HeaderMap::new();
+
+        //let mut headers = HeaderMap::new();
+        let mut headers = self.headers.clone();
         for (key, value) in &self.headers {
             let header_name = HeaderName::from_lowercase(&key.to_owned().into_bytes())?;
             let header_value = HeaderValue::from_str(&value.clone())?;
-            headers.insert(header_name, header_value);
+            headers.push((
+                header_name.to_string(),
+                header_value.to_str().unwrap().to_owned(),
+            ));
         }
+
+        // If the test case is authorized and a JWT token is provided, add it to the headers
+        if self.is_authorized {
+            if let Some(jwt_token) = jwt_token {
+                headers.push((
+                    "authorization".to_owned(),
+                    format!("Bearer {}", jwt_token.to_string()),
+                ));
+            }
+        }
+
+        self.headers = headers;
+
+        // convert headers vector into header map
+        let headers: HeaderMap = self
+            .headers
+            .iter()
+            .map(|(key, value)| {
+                let header_name = HeaderName::from_lowercase(&key.to_owned().into_bytes()).unwrap();
+                let header_value = HeaderValue::from_str(&value.clone()).unwrap();
+                (header_name, header_value)
+            })
+            .collect();
+
         let response = match self.method {
             Method::GET => client.get(self.url.as_str()).headers(headers).send()?,
             Method::POST => client
@@ -109,14 +154,27 @@ impl TestCase {
             _ => {
                 eprintln!("Unsupported method: {:?}", self.method);
                 self.actual_status = Some(StatusCode::METHOD_NOT_ALLOWED);
-                return Ok(());
+                return Ok(None);
             }
         };
+
         self.actual_status = Some(response.status());
-        self.response = response.json()?;
+        let response_text = response.text()?;
+
+        // Parse the response text as json
+        let cloned_response = serde_json::from_str(&response_text)?;
+        self.response = cloned_response;
+
+        // Add code to extract JWT token from response
+        // if the test case is an authorizer
+        let jwt_token: Option<String> = if self.is_authorizer {
+            Some(extract_jwt_token_from_response(&response_text)?)
+        } else {
+            None
+        };
 
         self.print_result();
-        Ok(())
+        Ok(jwt_token)
     }
 
     pub fn print_result(&self) {
@@ -126,10 +184,15 @@ impl TestCase {
         println!("Then: {:?}", self.then);
         println!("Expected: {}", self.expected_status);
         println!(
+            "Method: {:?}, URL: {}, Headers: {:?}, Payload: {:?}",
+            self.method, self.url, self.headers, self.payload
+        );
+        println!(
             "Actual: {}",
             self.actual_status
                 .unwrap_or_else(|| StatusCode::from_u16(0).unwrap())
         );
+        println!("Response: {:#?}", self.response);
         if self.expected_status
             == self
                 .actual_status
@@ -143,7 +206,12 @@ impl TestCase {
     }
 }
 
-#[derive(Debug)]
-pub struct TestGroup {
-    pub cases: Vec<TestCase>,
+#[derive(Deserialize)]
+struct TokenResponse {
+    token: String,
+}
+
+fn extract_jwt_token_from_response(response: &str) -> Result<String, Box<dyn Error>> {
+    let token_response: TokenResponse = serde_json::from_str(response)?;
+    Ok(token_response.token)
 }
--- src/test_suite.rs
+++ src/test_suite.rs
@@ -3,18 +3,28 @@ use crate::test_case::TestCase;
 
 pub struct TestSuite {
     test_cases: Vec<TestCase>,
+    jwt_token: Option<String>,
 }
 
 impl TestSuite {
-    pub fn run(&mut self) {
+    pub fn run(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         println!("Running the test suite");
         println!("======================");
-        for mut test_case in &mut self.test_cases {
-            test_case.run();
+        for test_case in &mut self.test_cases {
+            let jwt_token = test_case.run(self.jwt_token.clone())?;
+            //print the value of jwt_token
+            println!("JWT token: {:?}", jwt_token);
+            if let Some(jwt_token) = jwt_token {
+                self.jwt_token = Some(jwt_token);
+            }
         }
+        Ok(())
     }
 
     pub fn new(test_cases: Vec<TestCase>) -> Self {
-        TestSuite { test_cases }
+        TestSuite {
+            test_cases,
+            jwt_token: None,
+        }
     }
 }
